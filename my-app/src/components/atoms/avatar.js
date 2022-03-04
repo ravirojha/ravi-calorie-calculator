dc0bb8fe8340af16e0c7ca210a4ca2ee2007676f
ravi ranjan <ravi.ranjan@bitcs.in>
2022-03-04 22:15:32 +0530

    added avatar

---
+++ my-app/src/components/atoms/avatar.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import styled from "styled-components";
+
+export default function AvatarIcon({handleLogout}) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <StyledComponent>
+        <Toolbar className='avatar'>
+                <div>
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleMenu}
+                        color="inherit"
+                    >
+                        <AccountCircle fontSize="large"/>
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>Settings</MenuItem>
+                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                    </Menu>
+                </div>
+        </Toolbar>
+        </StyledComponent>
+    );
+}
+
+const StyledComponent = styled.div`
+.avatar {
+    position: fixed;
+    right: 5%;
+    top: 0.9%;
+}
+`;
